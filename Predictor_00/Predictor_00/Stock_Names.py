b98d8ac993e7f1626dfde510aa895d765ae90a0e
Derbs <derbi.e.calderon@gmail.com>
2021-04-09 13:50:17 -0700

    Add files via upload

---
+++ Predictor_00/Predictor_00/Stock_Names.py
@@ -0,0 +1,15 @@
+def st_names():
+    names = ['ADP', 'SPY', 'BA', 'KSU', 'GME', 'MJ', 'LODE', 'CLF', 'TMUS', 'AXP', 'ABBV', 'KR', 'VZ',
+             'MMC', 'CVX', 'MRK', 'BMY', 'TKAT', 'CLVS', 'BIDU', 'VIAC',
+             'PRQR', 'MU', 'MARA', 'TSLA', 'AMC', 'AAPL', 'GOLD', 'GM', 'CVX', 'KR', 'PFE', 'BMY', 'PFE', 'USB',
+             'RH', 'WFC', 'V', 'MSFT', 'AMZN', 'FB', 'GOOG', 'JPM', 'UNH', 'NVDA', 'DIS', 'HD', 'PG', 'MA', 'BAC',
+             'PYPL', 'INTC', 'CMCSA', 'NFLX', 'XOM', 'ADBE', 'CSCO', 'T', 'ABT', 'KO', 'CRM', 'CVX', 'AVGO',
+             'PEP', 'WMT', 'TMO', 'TXN', 'ACN', 'NKE', 'MCD',
+             'MDT', 'COST', 'QCOM', 'HON', 'C', 'NEE', 'UNP', 'LIN', 'LLY',
+             'AMGN', 'DHR', 'LOW', 'BMY', 'PM', 'ORCL', 'AMAT', 'SBUX',
+             'CAT', 'UPS', 'IBM', 'RTX', 'GE', 'DE', 'MS',
+             'MMM', 'GS', 'BLK', 'INTU', 'AMT', 'MU', 'TGT', 'SCHW', 'NOW', 'BKNG', 'AMD', 'CVS',
+             'MO', 'AXP', 'LRCX', 'LMT', 'FIS', 'ISRG', 'SPGI', 'ANTM', 'CHTR', 'CI', 'GILD', 'MDLZ', 'ADP',
+             'TJX', 'SYK', 'PLD', 'TFC', 'USB', 'CCI', 'ATVI', 'PNC', 'ZTS', 'CSX', 'DUK']
+
+    return names
